9fcfbf8f1888f666bb153d826448b2b0a085446f
ngocla99 <ngoc.leanh1999@gmail.com>
2025-09-03 20:20:34 +0700

    chore: update project dependencies and modify application metadata

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,27 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanstackDevtools } from '@tanstack/react-devtools'
+import { TanstackDevtools } from "@tanstack/react-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-import Header from '../components/Header'
+import { Header } from "@/components/header";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Header />
-      <Outlet />
-      <TanstackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
-  ),
-})
+	component: () => (
+		<>
+			<Header />
+			<Outlet />
+			{import.meta.env.DEV && (
+				<TanstackDevtools
+					config={{
+						position: "bottom-left",
+					}}
+					plugins={[
+						{
+							name: "Tanstack Router",
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+					]}
+				/>
+			)}
+		</>
+	),
+});
